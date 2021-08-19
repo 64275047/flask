399924a322ae610a13d8bf074093f90be0fa23a7
jinyang <jinyang@youyuan.com>
2021-08-19 16:58:01 +0800

    1

--- web.py
+++ web.py
@@ -1,13 +1,21 @@
+# coding:utf-8
 
-from flask import Flask
+from flask import Flask,render_template,request,redirect,url_for
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
- 
 
-@app.route('/hi')
-def hello_world():
-    return 'Hello World!'
-
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+  if request.method == 'POST':
+    f = request.files['file']
+    basepath = os.path.dirname(__file__) # 当前文件所在路径
+    upload_path = os.path.join(basepath, '/tmp/upload',secure_filename(f.filename)) #注意：没有的文件夹一定要先创建，不然会提示没有该路径
+    f.save(upload_path)
+    return redirect(url_for('upload'))
+  return render_template('upload.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=8080)
+  #app.run(debug=True)
+  app.run(host='172.18.8.58',port=8080)
